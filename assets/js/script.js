d066563932efc6c7860ab0573ee17ada11ba59f2
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-11-12 18:08:06 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -166,7 +166,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -205,16 +205,19 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+    
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("drover");
+    $(".bottom-trash").remove("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     var tempArr =[];
@@ -259,12 +262,13 @@ $("#trash").droppable({
   drop: function(event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log("out");
+   $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -272,6 +276,10 @@ $("#trash").droppable({
 $("#modalDueDate").datepicker({
   minDate:1
 });
+
+setInterval(function() {
+  // code to execute
+}, (1000 * 60)* 30);
 
 // load tasks for the first time
 loadTasks();
